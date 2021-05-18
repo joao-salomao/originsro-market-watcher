4598e64930bb4ac0160cd76a16429d8ff2314b34
João Salomão <joaosalomao.613@gmail.com>
2021-05-17 23:51:10 -0300

    Importando estilos do bootstrap no componente principal da aplicação

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,14 +1,9 @@
 <template>
-    <div>
-        <h1>Vue Router Demo App</h1>
-
-        <p>
-            <router-link :to="{ name: 'home' }">Home</router-link> |
-            <router-link :to="{ name: 'hello' }">Hello World</router-link>
-        </p>
-
-        <div class="container">
-            <router-view></router-view>
-        </div>
-    </div>
+  <div>
+    <h1 class="text-center">Ragnarok Market Watch</h1>
+    <router-view></router-view>
+  </div>
 </template>
+<style lang="scss">
+@import "../sass/app.scss";
+</style>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
+  <div class="p-4">
     <h1>Items</h1>
-    <div>
-      <div class="b-card d-flex" :key="item.id" v-for="item in items">
-        <span>{{ item.item_id }}</span>
-        <img :src="item.icon" />
-        <span>
-          {{ item.name }}
-        </span>
-      </div>
-      <button @click="onClickPaginationAction(firstPageUrl)">First</button>
-      <button @click="onClickPaginationAction(firstPageUrl)">Previous</button>
-      <button @click="onClickPaginationAction(nextPageUrl)">Next</button>
-    </div>
+    <b-table striped hover :items="items"></b-table>
+    <!-- <b-card
+      :key="item.id"
+      v-for="item in items"
+      class="d-flex justify-content-between"
+    >
+      <div class="d-flex justify-content-between text-center"></div>
+    </b-card> -->
+    <button @click="onClickPaginationAction(firstPageUrl)">First</button>
+    <button @click="onClickPaginationAction(firstPageUrl)">Previous</button>
+    <button @click="onClickPaginationAction(nextPageUrl)">Next</button>
   </div>
 </template>
 <script>
