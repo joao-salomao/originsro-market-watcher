cc92775b5a2a5a4888d7d16f0e82e40ddf19307e
João Salomão <joaosalomao.613@gmail.com>
2021-05-23 18:08:56 -0300

    Adicionando opção de filtrar por item_id e nome

--- resources/js/views/Items.vue
+++ resources/js/views/Items.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <div>
-      <b-form-select
-        class="per-page"
-        :options="perPageOptions"
-        v-model="pagination.perPage"
-      />
+      <div>
+        <div class="filters">
+          <div class="mr-2">
+            <span>Item ID:</span>
+            <b-form-input
+              v-model="filters.itemId"
+              class="input"
+              type="number"
+              debounce="1000"
+              size="sm"
+            />
+          </div>
+          <div class="mr-2">
+            <span>Name:</span>
+            <b-form-input
+              v-model="filters.name"
+              class="input"
+              type="text"
+              debounce="1000"
+              size="sm"
+            />
+          </div>
+          <b-form-select
+            size="sm"
+            class="per-page"
+            :options="perPageOptions"
+            v-model="pagination.perPage"
+          />
+        </div>
+      </div>
       <b-table
         hover
         striped
@@ -68,7 +93,12 @@ export default {
         perPage: 10,
         currentPage: 1,
       },
+      filters: {
+        name: null,
+        itemId: null,
+      },
       perPageOptions: [10, 15, 30, 50, 100, 200],
+
       fields: [
         {
           key: "item_id",
@@ -78,17 +108,17 @@ export default {
           sortable: true,
         },
         {
-          key: "icon",
-          label: "Icon",
-          thClass: "text-center",
-          tdClass: "text-center",
-        },
-        {
           key: "name",
           label: "Name",
           thClass: "text-center",
           tdClass: "text-center",
           sortable: true,
+        },
+        {
+          key: "icon",
+          label: "Icon",
+          thClass: "text-center",
+          tdClass: "text-center",
         },
         {
           key: "npc_price",
@@ -107,15 +137,24 @@ export default {
     };
   },
   watch: {
-    "pagination.perPage": function (newValue, oldValue) {
-      if (newValue != oldValue) {
+    requestParams(newValue, oldValue) {
+      const isDifferent = Object.keys(newValue).some((key) => {
+        return newValue[key] != oldValue[key];
+      });
+
+      if (isDifferent) {
         this.getItems();
       }
     },
-    "pagination.currentPage": function (newValue, oldValue) {
-      if (newValue != oldValue) {
-        this.getItems();
-      }
+  },
+  computed: {
+    requestParams() {
+      return {
+        name: this.filters.name,
+        item_id: this.filters.itemId,
+        page: this.pagination.currentPage,
+        per_page: this.pagination.perPage,
+      };
     },
   },
   mounted() {
@@ -149,8 +188,7 @@ export default {
       api
         .get("api/item", {
           params: {
-            page: this.pagination.currentPage,
-            per_page: this.pagination.perPage,
+            ...this.requestParams,
           },
         })
         .then((resp) => {
@@ -170,10 +208,20 @@ export default {
 };
 </script>
 <style scoped>
+.filters {
+  display: flex;
+  margin-bottom: 0.5rem;
+  justify-content: flex-end;
+}
+
 .per-page {
+  align-self: flex-end;
   float: right;
   max-width: 70px;
   border-radius: 30px;
-  margin-bottom: 0.5rem;
+}
+
+.input {
+  border-radius: 30px;
 }
 </style>
