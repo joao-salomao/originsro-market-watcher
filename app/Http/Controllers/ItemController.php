10e7e21feef995a963ef910fd48900867d64fe96
João Salomão <joaosalomao.613@gmail.com>
2021-05-23 18:06:40 -0300

    Adicionando filtros dinâmicos na query

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -9,8 +9,18 @@ class ItemController extends Controller
 {
     public function index(Request $request)
     {
+        $query = Item::orderBy('item_id', 'asc');
+
+        foreach (['name', 'item_id'] as $filterKey) {
+            $filterValue = $request->get($filterKey, null);
+            $isNotEmpty = !empty($filterValue);
+            if ($isNotEmpty) {
+                $query->where($filterKey, 'like', '%' . $filterValue . '%');
+            }
+        }
+
         $perPage = $request->get('per_page', 10);
-        $items = Item::orderBy('item_id', 'asc')->paginate($perPage);
+        $items = $query->paginate($perPage);
         return response($items);
     }
 }
