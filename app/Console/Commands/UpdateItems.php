c443821b8ea4f61ef6556f9d9ac764fc2e74fbff
João Salomão <joaosalomao.613@gmail.com>
2021-05-23 02:05:14 -0300

    Adicionando opção use-backup para usar dados da api ou do backup

--- app/Console/Commands/UpdateItems.php
+++ app/Console/Commands/UpdateItems.php
@@ -8,7 +8,7 @@ use App\Http\Services\OriginsRoApiClient;
 
 class UpdateItems extends Command
 {
-    protected $signature = 'ragnarok:update-items';
+    protected $signature = 'ragnarok:update-items {--use-backup}';
     protected $description = 'Update items table';
 
     public function __construct()
@@ -19,10 +19,31 @@ class UpdateItems extends Command
 
     public function handle()
     {
-        $items = (new OriginsRoApiClient)->getItemsList();
+        $items = $this->getItems();
         foreach ($items as $item) {
             $this->createOrUpdateItem($item);
         }
+    }
+
+    private function getItems(): array
+    {
+        if ($this->option('use-backup')) {
+            return $this->getItemsFromBackup();
+        }
+        return $this->getItemsFromApi();
+    }
+
+    private function getItemsFromBackup(): array
+    {
+        $path = storage_path() . '\items.json';
+        $content = file_get_contents($path);
+        $data = json_decode($content, true);
+        return $data['items'];
+    }
+
+    private function getItemsFromApi(): array
+    {
+        return (new OriginsRoApiClient)->getItemsList();
     }
 
     private function createOrUpdateItem(array $item)
