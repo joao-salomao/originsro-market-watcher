8e3f6cd27b3d18f68d5c08c2676879744455c8cc
João Salomão <joaosalomao.613@gmail.com>
2021-05-23 02:11:06 -0300

    Adicionando trait SoftDeletes na model Alert

--- app/Model/Alert.php
+++ app/Model/Alert.php
@@ -3,9 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Alert extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'item_id',
         'max_price'
