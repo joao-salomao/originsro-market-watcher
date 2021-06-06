c08410f1241cd11af4b319d5a2780976eaba7781
João Salomão <joaosalomao.613@gmail.com>
2021-06-06 02:28:13 -0300

    centralizando controle de autenticação no store

--- resources/js/services/store.js
+++ resources/js/services/store.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import api from "./api";
 
 export default new Vue({
-    data: {
-        user: {
-            isAuthenticated: false
+    data() {
+        const token = localStorage.getItem('token')
+        return {
+            user: {
+                token,
+                isAuthenticated: token !== null
+            }
+        }
+    },
+    watch: {
+        'user.token': function (newValue) {
+            localStorage.setItem("token", newValue);
+            api.defaults.headers.common["Authorization"] = newValue;
+            this.user.isAuthenticated = newValue !== null;
         }
     }
 })
