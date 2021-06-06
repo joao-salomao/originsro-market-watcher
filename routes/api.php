776dd85e2cc0271a9352c698656fb3375d0432cb
João Salomão <joaosalomao.613@gmail.com>
2021-06-06 17:17:13 -0300

    Criando rota para pegar usuário logado

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
+    Route::get('user/current', function () {
+        return response(auth()->user());
+    });
+
     Route::get('item', 'ItemController@index');
     Route::resource('alert', AlertController::class);
 });
