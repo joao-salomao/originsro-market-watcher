137d5bd9d415c38b23ce71cea8140658bd0f7bcc
João Salomão <joaosalomao.613@gmail.com>
2021-06-06 03:27:36 -0300

    deixando somente os dados no store

--- resources/js/services/store.js
+++ resources/js/services/store.js
@@ -1,21 +1,8 @@
 import Vue from 'vue'
-import api from "./api";
-
 export default new Vue({
-    data() {
-        const token = localStorage.getItem('token')
-        return {
-            user: {
-                token,
-                isAuthenticated: token !== null
-            }
-        }
-    },
-    watch: {
-        'user.token': function (newValue) {
-            localStorage.setItem("token", newValue);
-            api.defaults.headers.common["Authorization"] = newValue;
-            this.user.isAuthenticated = newValue !== null;
+    data: {
+        user: {
+            isAuthenticated: localStorage.getItem('token') !== null
         }
     }
 })
