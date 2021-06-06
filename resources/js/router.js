50be90ed5b2120266e55970b7b71a18f9e881d66
João Salomão <joaosalomao.613@gmail.com>
2021-06-06 19:51:19 -0300

    Corrigindo aplicação do redirecionamento

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router'
 import Login from './views/Login'
 import Items from './views/Items'
 import Alerts from './views/Alerts'
+import Register from './views/Register'
 import store from './services/store'
 
 const router = new VueRouter({
@@ -15,20 +16,27 @@ const router = new VueRouter({
         {
             path: '/items',
             name: 'Items',
-            component: Items
+            component: Items,
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            component: Register
         },
         {
             path: '/alerts',
             name: 'Alerts',
-            component: Alerts
+            component: Alerts,
+            meta: { requiresAuth: true }
         }
     ],
 })
 
 router.beforeEach((to, _, next) => {
-    if (to.name !== 'Login' && !store.user.isAuthenticated) {
+    if (to.matched.some(record => record.meta.requiresAuth) && !store.user.isAuthenticated) {
         next({ name: 'Login' })
-    } else if ((to.name === 'Login' || !to.name) && store.user.isAuthenticated) {
+    } else if (!to.name) {
         next({ name: 'Items' })
     } else {
         next()
